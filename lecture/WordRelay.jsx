b3206f67f640612ac20ac475e4d5ee62fd9c216d
wrkbr <zcarc1@gmail.com>
2019-12-04 04:36:23 +0900

    2-5. 웹팩으로 빌드하기

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -3,10 +3,11 @@ const {Component} = React;
 
 class WordRelay extends Component {
     state = {
-
+        text: 'Hello Webpack',
     };
 
     render() {
+        return <h1>{this.state.text}</h1>;
 
     }
 }
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -13,6 +13,22 @@ module.exports = {
     entry: {
         app: ['./client'],
     },// 입력
+
+    // test: /.jsx?/, 이 부분은 js와 jsx 파일에 대해 룰을 적용하겠다 라는 의미
+    // loader: 'bable-loader' 바벨 로더라는 룰을 적용 하겠다 라는 의미
+    module: {
+        rules: [{
+            test: /.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+            }
+        }]
+    },
+
+    // __dirname 은 노드에서 기본적으로 제공해주고 해당 파일의 폴더 경로를 반환해준다.
+    // path.join()은 전달된 인자끼리 합치고 path 경로를 만들어서 반환해준다.
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js'
