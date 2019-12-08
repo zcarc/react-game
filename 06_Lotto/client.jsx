f5e1983dbdb16d290d0869d2f25b495df570ade7
wrkbr <zcarc1@gmail.com>
2019-12-08 20:34:37 +0900

    6-1. 로또 추첨기 컴포넌트

---
+++ 06_Lotto/client.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {hot} from 'react-hot-loader/root';
+import Lotto from "./Lotto";
+const Hot = hot(Lotto);
+
+
+ReactDOM.render(<Hot />, document.querySelector('#root'));
+
